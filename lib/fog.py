b98dc68d915e97f3545a404a8d59b728f4adeacf
José L. Risco Martín <jlrisco@ucm.es>
2023-02-01 11:51:07 +0100

    Still with H5 files

--- lib/fog.py
+++ lib/fog.py
@@ -49,11 +49,48 @@ class FogServer(Atomic):
 
     def generate_h5_file(self, data_center_name: str, fog_server_name: str, start_dt: datetime.datetime, stop_dt: datetime.datetime, step: int):
         """Generate h5 file with the data for the prediction."""
-        # TODO: Continue here
+        # Initialize variables
         current_dt = start_dt
+        current_day = -1
         sensor_files = {}
+        current_data = {}
+        # Open files
         for sensor_name in self.sensor_names:
-            sensor_files[sensor_name] = open(f"data/output/{data_center_name}/{fog_server_name}/{sensor_name}.csv", mode='r')
+            sensor_files[sensor_name] = open(f"data/output/{data_center_name}/{sensor_name}.csv", mode='r')
+            # Read the header
+            sensor_files[sensor_name].readline()
+            current_data[sensor_name] = 0
+        # Prepare the H5 file
+        h5 = tb.open_file('data/output/example.h5', 'w')
+        group_fog = h5.create_group("/", data_center_name)
+        group_fog = h5.create_group(group_fog, fog_server_name)
+        # Loop over the time
         while current_dt < stop_dt:
-
+            aux_day = current_dt.day
+            if aux_day != current_day:
+                current_day = aux_day
+                group_day = h5.create_group(group_fog, current_dt.strftime("%Y-%m-%d"))
+            # Read data from files
+            for sensor_name in self.sensor_names:
+                sensor_dt = datetime.datetime.strptime("1900-01-01 10:00:00", "%Y-%m-%d %H:%M:%S")
+                while sensor_dt<current_dt:
+                    line = sensor_files[sensor_name].readline()
+                    if line != '':
+                        sensor_dt = datetime.datetime.strptime(line.split(',')[1], "%Y-%m-%d %H:%M:%S")
+                    else:
+                        break
+                if line != '':
+                    current_data[sensor_name] = float(line.split(',')[2])
+            # Prepare the new row
+            row: list = []
+            row.append(current_dt.timestamp)
+            for sensor_name in self.sensor_names:
+                row.append(current_data[sensor_name])
+            # Append the new row to the group_day
+            h5.get_node(group_fog, group_day).append(row)
             current_dt += datetime.timedelta(seconds=step)
+        # Close files
+        for sensor_name in self.sensor_names:
+            sensor_files[sensor_name].close()
+        # Close the H5 file
+        h5.close()
